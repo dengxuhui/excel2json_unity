4f72c30cc8e38224095fd4c776d3bb355dfc70f1
dengxuhui <419270388@qq.com>
2023-05-25 17:57:50 +0800

    引入了tmp，增加示例工程

--- excel2json_unity/Assets/Sample/Scripts/Main.cs
+++ excel2json_unity/Assets/Sample/Scripts/Main.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Excel2JsonUnity;
 using Excel2JsonUnity.Sample;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        var type = typeof(SampleBaseConfig);
-        var fullName = type.FullName;
-        Debug.Log(fullName);
-        var ab = Assembly.GetAssembly(type);
-        Debug.Log(ab.FullName);
+#if UNITY_EDITOR
+        var asset = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Sample/Json/SampleXlsx.json");
+        var configs = JsonConvert.DeserializeObject<List<SampleXlsx>>(asset.text);
+        if (configs == null)
+        {
+            return;
+        }
+
+        var id0 = configs[0];
+        text.text = $"Name:{id0.Data.Name} Age:{id0.Data.Age}";
+#endif
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
